af87188981b97cb8a40c2e174789a79e07f4c1a9
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-07-11 20:57:58 -0400

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -1139,16 +1139,12 @@ exec_new_vmspace(imgp, sv)
 		    MAP_INHERIT_SHARE | MAP_ACC_NO_CHARGE);
 		if (error != KERN_SUCCESS) {
 			vm_object_deallocate(obj);
-<<<<<<< HEAD
 #ifdef PAX_ASLR
 			pax_log_aslr(p, PAX_LOG_DEFAULT,
 			    "failed to map the shared-page @%p",
 			    (void *)p->p_shared_page_base);
 #endif
-			return (error);
-=======
 			return (vm_mmap_to_errno(error));
->>>>>>> origin/freebsd/11.1-releng/master
 		}
 
 		p->p_timekeep_base = sv->sv_timekeep_base;
@@ -1177,7 +1173,6 @@ exec_new_vmspace(imgp, sv)
 	} else {
 		ssiz = maxssiz;
 	}
-<<<<<<< HEAD
 
 	stack_addr = sv->sv_usrstack;
 #ifdef PAX_ASLR
@@ -1193,23 +1188,16 @@ exec_new_vmspace(imgp, sv)
 #ifdef PAX_NOEXEC
 	pax_noexec_nx(p, &stackprot, &stackmaxprot);
 #endif
-	error = vm_map_stack(map, stack_addr, (vm_size_t)ssiz, stackprot, stackmaxprot, MAP_STACK_GROWS_DOWN);
+	error = vm_map_stack(map, stack_addr, (vm_size_t)ssiz,
+	    stackprot, stackmaxprot, MAP_STACK_GROWS_DOWN);
 	if (error) {
 #ifdef PAX_ASLR
 		pax_log_aslr(p, PAX_LOG_DEFAULT,
 		    "failed to map the main stack @%p",
 		    (void *)p->p_usrstack);
 #endif
-		return (error);
-	}
-=======
-	stack_addr = sv->sv_usrstack - ssiz;
-	error = vm_map_stack(map, stack_addr, (vm_size_t)ssiz,
-	    obj != NULL && imgp->stack_prot != 0 ? imgp->stack_prot :
-	    sv->sv_stackprot, VM_PROT_ALL, MAP_STACK_GROWS_DOWN);
-	if (error != KERN_SUCCESS)
 		return (vm_mmap_to_errno(error));
->>>>>>> origin/freebsd/11.1-releng/master
+	}
 
 	/*
 	 * vm_ssize and vm_maxsaddr are somewhat antiquated concepts, but they
